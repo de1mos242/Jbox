67051cefaef5f2df170e86d8206d45347488cc3f
De1mos <de1m0s242@gmail.com>
2014-10-22 00:34:43 +0800

    check failed test

---
+++ web-client/src/test/java/net/de1mos/jukebox/webclient/FailedTest.java
@@ -0,0 +1,12 @@
+package net.de1mos.jukebox.webclient;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FailedTest {
+
+	@Test
+	public void testFail() {
+		Assert.assertFalse(true);
+	}
+}
